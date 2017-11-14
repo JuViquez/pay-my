0f6ddfbbcc552b266dbd202ec6ddf64d1b63d1c3
JuViquez <jviquez2@gmail.com>
2017-11-14 01:30:29 -0600

    user component

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -5,13 +5,25 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  providers: [UserService]
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService
+     .getUsers()
+     .then((users: User[]) => { 
+      this.users = users;
+    })
+     
+    }
+ }
 
 
-  constructor() { }
-
-}
 
 
