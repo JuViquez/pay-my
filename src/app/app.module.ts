545ee28dbc7c387a383a0808d318a9886e19bd2b
Marvin Calvo <kmarvin14@gmail.com>
2017-11-14 23:53:26 -0600

    Implementation of application routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routes } from './app.router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
@@ -10,8 +10,6 @@ import { UserListComponent } from './users/user-list/user-list.component';
 import { CurrentBillComponent } from './bills/current-bill/current-bill.component';
 import { BillLineListComponent } from './bill_lines/bill-line-list/bill-line-list.component';
 import { MapComponent } from './business/map/map.component';
-import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
-
 
 @NgModule({
   declarations: [
@@ -25,12 +23,12 @@ import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
     BrowserModule,
     FormsModule,
     HttpModule,
+    routes,
     ReactiveFormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyC1nrxG7aYykMQZQuSSosEdhFAzBQswtXQ',
       libraries: ["places"],
     }),
-    AgmSnazzyInfoWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent, UserListComponent]
